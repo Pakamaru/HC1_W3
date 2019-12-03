feb3f49f4fffd30dcbcb63d73a95b6f16040b83e
Amaru <sanamaru99@gmail.com>
2019-12-03 09:41:28 +0100

    Add project files.

---
+++ HC1_W3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HC1_W3
+{
+    struct Student
+    {
+        public string Naam { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Naam = "Jan";
+            VeranderNaam(s);
+            Console.WriteLine(s.Naam);
+        }
+
+        public static void VeranderNaam(Student student)
+        {
+            student.Naam = "Bob";
+        }
+    }
+}
